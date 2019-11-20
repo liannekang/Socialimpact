ae7b906a432f4cd0fc544d801287b2fc71e24e19
liannekang <36962211+liannekang@users.noreply.github.com>
2019-11-20 09:55:06 -0600

    Add files via upload

---
+++ medschool_2/js/script.js
@@ -0,0 +1,34 @@
+// $(function(){
+//         slider = $("#slider1").slideReveal({
+//           // width: 100,
+//           // push: false,
+//           position: "left",
+//           speed: 600,
+//           trigger: $("#trigger1"),
+//           // autoEscape: false,
+//           show: function(obj){
+//             console.log(obj);
+//           },
+//           shown: function(obj){
+//             console.log(obj);
+//           },
+//           hide: function(obj){
+//             console.log(obj);
+//           },
+//           hidden: function(obj){
+//             console.log(obj);
+//           }
+//         });
+
+      
+// });
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
